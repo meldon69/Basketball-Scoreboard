027c117b948382ec0e4ca371e11e1f74eadd65f1
meldon69 <102337958+meldon69@users.noreply.github.com>
2022-06-29 17:06:32 +1200

    Add files via upload

--- Basketball Scoreboard/index.js
+++ Basketball Scoreboard/index.js
@@ -8,12 +8,15 @@ let guestDisplay = document.querySelector("#guest-display")
 let homeFouls = document.querySelector("#home-fouls")
 let guestFouls = document.querySelector("#guest-fouls")
 let period = document.querySelector("#period")
+let homeTeam = document.querySelector("#home-team")
+let guestTeam = document.querySelector("#guest-team")
 
 period.textContent = quarter
+
 function plusOne() {
-homeScore += 1
-homeDisplay.textContent = homeScore
-winner()
+    homeScore += 1
+    homeDisplay.textContent = homeScore
+    winner()
 }
 
 function plusTwo() {
@@ -37,43 +40,57 @@ function plusOneGuest() {
     guestScore += 1
     guestDisplay.textContent = guestScore
     winner()
+}
+    
+function plusTwoGuest() {
+    guestScore += 2
+    guestDisplay.textContent = guestScore
+    winner()
+}
+    
+function plusThreeGuest() {
+    guestScore += 3
+    guestDisplay.textContent = guestScore
+    winner()
+}
+
+function guestFoul() {
+    foulsGuest +=1
+    guestFouls.textContent = foulsGuest
+}
+
+function newGame() {
+    foulsHome = 0
+    homeScore = 0
+    guestScore = 0
+    foulsGuest = 0
+    quarter = 1
+    homeDisplay.textContent = homeScore
+    homeFouls.textContent = foulsHome
+    guestDisplay.textContent = guestScore
+    guestFouls.textContent = foulsGuest
+    period.textContent = quarter        
+    document.querySelector("#home-display").style.color = "Red"
+    document.querySelector("#guest-display").style.color = "Red"
+    
+}
+
+function nextQuarter() {
+    period.textContent = quarter += 1
+    if (quarter > 4) {
+    return period.textContent = 4
+    }
     }
     
-    function plusTwoGuest() {
-        guestScore += 2
-        guestDisplay.textContent = guestScore
-        winner()
+function winner() {
+    if (homeScore > guestScore) {
+        document.querySelector("#home-display").style.color = "Red"
+        document.querySelector("#guest-display").style.color = "White"                
+    } else if (guestScore > homeScore) {
+        document.querySelector("#home-display").style.color = "White"
+        document.querySelector("#guest-display").style.color = "Red"
+    } else if (homeScore === guestScore) {    
+    document.querySelector("#home-display").style.color = "Red"
+        document.querySelector("#guest-display").style.color = "Red"
     }
-    
-    function plusThreeGuest() {
-        guestScore += 3
-        guestDisplay.textContent = guestScore
-        winner()
-    }
-
-    function guestFoul() {
-        foulsGuest +=1
-        guestFouls.textContent = foulsGuest
-    }
-
-    function newGame() {
-        homeScore = 0
-        foulsHome = 0
-        guestScore = 0
-        foulsGuest = 0
-        quarter = 1
-        homeDisplay.textContent = homeScore
-        homeFouls.textContent = foulsHome
-        guestDisplay.textContent = guestScore
-        guestFouls.textContent = foulsGuest
-        period.textContent = quarter
-
-    }
-
-    function nextQuarter() {
-        period.textContent = quarter += 1
-        if (quarter > 4) {
-            return period.textContent = 4
-        }
-    }
-    
+}
